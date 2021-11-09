917d3f0a012d2f9c22c543eb2ca3c21c4c3a97be
Scott <hscottvo@gmail.com>
2021-11-08 23:02:33 -0800

    Implemented single triangle shading to white

--- driver_state.cpp
+++ driver_state.cpp
@@ -20,7 +20,7 @@ void initialize_render(driver_state& state, int width, int height)
     state.image_height=height;
     state.image_color=0;
     state.image_depth=0;
-    // std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
+    std::cout<<"TODO: allocate and initialize state.image_depth."<<std::endl;
     state.image_color = new pixel[width*height];
     for (unsigned int i = 0; i < width*height; ++i){
         state.image_color[i] = make_pixel(0, 0, 0);
@@ -36,6 +36,47 @@ void initialize_render(driver_state& state, int width, int height)
 //   render_type::strip -    The vertices are to be interpreted as a triangle strip.
 void render(driver_state& state, render_type type)
 {
+    data_geometry* geometry_arr = new data_geometry[state.num_vertices];
+    for (unsigned int i = 0; i < state.num_vertices; ++i) {
+        geometry_arr[i].data = new float[MAX_FLOATS_PER_VERTEX];
+        data_vertex vertex;
+        // std::cout << "TODO: Check if vertex.data is correct" << std::endl;
+        vertex.data = &(state.vertex_data[i * state.floats_per_vertex]);
+        state.vertex_shader(vertex, geometry_arr[i], state.uniform_data);
+        // geometry_arr[i].gl_Position = 
+    }
+
+    switch(type){ 
+        case render_type::triangle:
+
+            for(unsigned int i = 0; i < state.num_vertices-2; i += 3) {
+            // for(unsigned int i = 0; i < state.num_vertices*state.floats_per_vertex; i += state.floats_per_vertex) {
+                // rasterize_triangle(state, 
+                //                    geometry_arr[i],
+                //                    geometry_arr[i+1],
+                //                    geometry_arr[i+2]);
+                clip_triangle(state, 
+                              geometry_arr[i],
+                              geometry_arr[i+1],
+                              geometry_arr[i+2],
+                              6);
+
+            }
+        break;
+
+        case render_type::indexed:
+
+        break;
+
+        case render_type::fan:
+
+        break;
+
+        case render_type::strip:
+
+        break;
+    }
+
     std::cout<<"TODO: implement rendering."<<std::endl;
 }
 
@@ -62,6 +103,60 @@ void clip_triangle(driver_state& state, const data_geometry& v0,
 void rasterize_triangle(driver_state& state, const data_geometry& v0,
     const data_geometry& v1, const data_geometry& v2)
 {
-    std::cout<<"TODO: implement rasterization"<<std::endl;
+    vec3 point_a = vec3(v0.gl_Position[0]/v0.gl_Position[3], 
+                        v0.gl_Position[1]/v0.gl_Position[3], 
+                        v0.gl_Position[2]/v0.gl_Position[3]);
+    point_a[0] = ((state.image_width-1)*((point_a[0]+1)/2)); 
+    point_a[1] = ((state.image_height-1)*((point_a[1]+1)/2));
+
+    vec3 point_b = vec3(v1.gl_Position[0]/v1.gl_Position[3], 
+                        v1.gl_Position[1]/v1.gl_Position[3], 
+                        v1.gl_Position[2]/v1.gl_Position[3]);
+    point_b[0] = ((state.image_width-1)*((point_b[0]+1)/2)); 
+    point_b[1] = ((state.image_height-1)*((point_b[1]+1)/2));
+
+    vec3 point_c = vec3(v2.gl_Position[0]/v2.gl_Position[3], 
+                        v2.gl_Position[1]/v2.gl_Position[3], 
+                        v2.gl_Position[2]/v2.gl_Position[3]);
+    point_c[0] = ((state.image_width-1)*((point_c[0]+1)/2));
+    point_c[1] = ((state.image_height-1)*((point_c[1]+1)/2));
+
+    // can get bounding box of triangle and iterate over that instead of the whole image
+    float total_area = 0.5 * ((point_b[0]*point_c[1] - point_c[0]*point_b[1]) + 
+                              (point_c[0]*point_a[1]-point_a[0]*point_c[1]) + 
+                              (point_a[0]*point_b[1]-point_b[0]*point_a[1]));
+    float alpha;
+    float beta;
+    float gamma;
+    int x_min = std::min(std::min(point_a[0], point_b[0]), point_c[0]);
+    int y_min = std::min(std::min(point_a[1], point_b[1]), point_c[1]);
+
+    int x_max = std::max(std::max(point_a[0], point_b[0]), point_c[0]);
+    int y_max = std::max(std::max(point_a[1], point_b[1]), point_c[1]);
+
+    // for (unsigned int i = 0; i < state.image_width; ++i) {
+    //     for (unsigned int j = 0; j < state.image_height; ++j) {
+    for (unsigned int i = x_min; i < x_max; ++i) {
+        for (unsigned int j = y_min; j < y_max; ++j) {
+            vec3 point_p = vec3(i, j, 0);
+            alpha = (0.5 * ((point_b[0]*point_c[1] - point_c[0]*point_b[1]) + 
+                         (point_c[0]*point_p[1]-point_p[0]*point_c[1]) + 
+                         (point_p[0]*point_b[1]-point_b[0]*point_p[1])) / total_area);
+            beta = (0.5 * ((point_p[0]*point_c[1] - point_c[0]*point_p[1]) + 
+                        (point_c[0]*point_a[1]-point_a[0]*point_c[1]) +
+                        (point_a[0]*point_p[1]-point_p[0]*point_a[1])) / total_area);
+            gamma = (0.5 * ((point_b[0]*point_p[1]-point_p[0]*point_b[1]) + 
+                        (point_p[0]*point_a[1]-point_a[0]*point_p[1]) +
+                        (point_a[0]*point_b[1]-point_b[0]*point_a[1])) / total_area);
+
+            
+
+            if (alpha > 0 && beta > 0 && gamma > 0) {
+                state.image_color[j*state.image_width+i] = make_pixel(255, 255, 255);
+            }
+        }
+    }
+    // std::cout<<"TODO: implement rasterization"<<std::endl;
+
 }
 
